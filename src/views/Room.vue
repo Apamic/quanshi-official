4109ba5400b0816d9154cfc25232381c3a7adda0
pccgreat <15102736920@163.com>
2023-02-03 18:09:52 +0800

    first

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,10 +1,6 @@
 <template>
     <div class="page">
-<<<<<<< HEAD:src/views/Room.vue
-        1111
-=======
       123
->>>>>>> 40fe187e7e1a1d462ec57023b128ae3b72a32cea:src/views/Login.vue
     </div>
 </template>
 
